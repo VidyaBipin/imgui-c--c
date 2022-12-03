923d59d2b23726a611069085215825dd49f61715
Dicky <Dicky.Qi@icloud.com>
2022-12-03 13:30:00 +0800

    fixed emscripten build

--- imgui_helper.cpp
+++ imgui_helper.cpp
@@ -48,6 +48,7 @@ struct IUnknown;
 
 #if defined(__EMSCRIPTEN__)
 #define IMGUI_IMPL_OPENGL_ES2               // Emscripten    -> GL ES 2, "#version 100"
+#define PATH_SETTINGS "/.config/"
 #endif
 
 #include <vector>
