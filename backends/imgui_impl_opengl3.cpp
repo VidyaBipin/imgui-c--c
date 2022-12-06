184673180e0f76b10f55cd6063b3d6d3246372bc
Dicky <Dicky.Qi@icloud.com>
2022-12-06 23:51:16 +0800

    update imgui

--- backends/imgui_impl_opengl3.cpp
+++ backends/imgui_impl_opengl3.cpp
@@ -112,6 +112,10 @@
 #pragma clang diagnostic ignored "-Wold-style-cast"     // warning: use of old-style cast
 #pragma clang diagnostic ignored "-Wsign-conversion"    // warning: implicit conversion changes signedness
 #endif
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-function-type"   // warning: cast between incompatible function types
+#endif
 
 // GL includes
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -963,6 +967,9 @@ std::string ImGui_ImplOpenGL3_GLLoaderName()
 }
 // Add By Dicky end
 
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
