03527b34dd333765e15ee02630dc1b99664680df
Dicky <Dicky.Qi@icloud.com>
2023-08-02 11:36:04 +0800

    1. revert opengl3 UseBufferSubData for Apple, it will cause rendering flash 2. change SDL_GL rendering depth to 32

--- apps/application/source/opengl3/entry_sdl2.cpp
+++ apps/application/source/opengl3/entry_sdl2.cpp
@@ -228,7 +228,7 @@ int main(int argc, char** argv)
 
     // Create window with graphics context
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     int window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
     if (property.resizable) window_flags |= SDL_WINDOW_RESIZABLE;
